3e51f4f074b6602fe517541ef5538bc3c39713f2
lisanet <>
2019-05-03 12:58:22 +0200

    some comments

--- xcodeant.sh
+++ xcodeant.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash -l
 
 #  xcodeant.sh
 #  appbundler-xcode
 #
 #  wrapper script to locate the ant binary, which might be installed in some custom location
+#  This is done by simply using a login shell which uses your usual PATH
+#  Created by Simone Karin Lehmann on 16.01.15.
 #
-#  Created by Simone Karin Lehmann on 03.01.15.
-#
-ANT=$(which ant)
-"$ANT" $*
+ant $*
+
